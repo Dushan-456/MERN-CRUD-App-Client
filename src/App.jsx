1ca2d13ed10d73fa70c9e8f116c1e70ef0d9de85
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-15 01:03:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
-import UsersTable from './Components/UsersTable'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SideNavBar from "./Components/SideNavBar";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Users from "./Components/Users/Users";
+import Settings from "./Components/Settings/Settings";
+import Products from "./Components/Products/Products";
+import Categories from "./Components/Categories/Categories";
 
 function App() {
-
-  return (
-    <>
-     <UsersTable/>
-    </>
-  )
+   return (
+      <BrowserRouter>
+         <div className="app_body">
+            <div className="main_container">
+               <div className="left_side">
+                  <SideNavBar />
+               </div>
+               <div className="right_side">
+                  <Routes>
+                     <Route path="/" element={<Dashboard />} />
+                     <Route path="/users" element={<Users />} />
+                     <Route path="/settings" element={<Settings />} />
+                     <Route path="/products" element={<Products />} />
+                     <Route path="/categories" element={<Categories />} />
+                  </Routes>
+               </div>
+            </div>
+         </div>
+      </BrowserRouter>
+   );
 }
 
-export default App
+export default App;
