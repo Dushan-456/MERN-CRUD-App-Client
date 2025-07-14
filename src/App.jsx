b0797c9dcecebecaea0b53475113de4909e6fc9d
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-14 22:34:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import UsersTable from './Components/UsersTable'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
