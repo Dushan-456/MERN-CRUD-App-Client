9d388dc186b87f62e2d27e1ec8ea4aa693e800b6
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-16 00:29:44 +0530

    Update Users.jsx

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import UsersTable from './UsersTable'
+import React from "react";
+import UsersTable from "./UsersTable";
+import "./Users.css";
+import Button from '@mui/material/Button';
+import { Link  } from 'react-router-dom';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
 
 function Users() {
-  return (
-    <div>
-      <UsersTable/>
-    </div>
-  )
+   return (
+      <div>
+         <div className="users_header">
+            <h2>All Users List</h2>
+            <Link to="/users/add" style={{ textDecoration: "none" }}>
+               <Button variant="contained" startIcon={<AddCircleOutlineIcon />}>
+                  Add New User
+               </Button>
+            </Link>
+         </div>
+         <UsersTable />
+      </div>
+   );
 }
 
-export default Users
+export default Users;
