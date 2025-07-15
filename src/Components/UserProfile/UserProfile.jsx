dc8bbd5297a7406d5907ef31799bf9cfd6d183f8
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-15 15:51:45 +0530

    Update UserProfile.jsx

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Typography, Card, CardContent, Avatar, CircularProgress } from '@mui/material';
+import "./UserProfile.css";
+import {IconButton} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
 
 function UserProfile() {
   const { id } = useParams();
@@ -22,33 +28,61 @@ function UserProfile() {
       };
       setUser(mockUser);
       setLoading(false);
-    }, 1000); // Simulate 1 second delay
+    }, 2000); // Simulate 1 second delay
   }, [id]);
 
   if (loading) {
-    return <CircularProgress />;
+    return <profile className="profile_loading">
+        <CircularProgress color="success" />;
+        </profile>
   }
 
   return (
-    <Card sx={{ maxWidth: 500, margin: '2rem auto', padding: 2 }}>
-      <CardContent style={{ textAlign: 'center' }}>
-        <Avatar
-          alt={user.name}
-          src={user.image}
-          sx={{ width: 100, height: 100, margin: '0 auto 1rem' }}
-        />
-        <Typography variant="h5">{user.name}</Typography>
-        <Typography variant="body1" color="text.secondary">
-          Email: {user.gmail}
+    <>
+    <div className="profile">
+            <div className="profile_left">
+                 <Avatar
+           alt={user.name}
+           src={user.image}
+           sx={{ width: 300, height: 300, margin: '0 auto 1rem' }}
+         />
+         <div>
+                  <IconButton   color="primary" >
+            <EditIcon />
+         </IconButton>
+         <IconButton color="error">
+             <DeleteIcon />
+         </IconButton>
+         </div>
+         
+         <Typography variant="h4">{user.name}</Typography>
+         <Typography variant="body1" color="text.secondary">
+           Email: {user.gmail}
         </Typography>
-        <Typography variant="body1" color="text.secondary">
-          Age: {user.age}
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-          Address: {user.address}
-        </Typography>
-      </CardContent>
-    </Card>
+    </div>
+    <div className="profile_right">
+        <div className="content">
+                        <Typography variant="h5">Personal Details</Typography> <br />
+                        <Typography variant="body1" color="text.secondary"> First Name: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Last Name: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Gender: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Date Of Birth: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Age: {user.gmail} </Typography>
+        </div>
+        <div className="content">
+            <Typography variant="h5">Contact Details</Typography><br />
+             <Typography variant="body1" color="text.secondary"> Email: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Mobile Number: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Facebook: {user.gmail} </Typography>
+                        <Typography variant="body1" color="text.secondary"> Linkdin: {user.gmail} </Typography>
+            
+            </div>
+   
+    </div>
+    </div>
+
+    </>
+
   );
 }
 
