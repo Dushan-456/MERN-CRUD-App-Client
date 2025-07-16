290e76b9900a0d1aae33ab7cf9fcfbbd8ce58425
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-16 18:09:12 +0530

    Update EdituserModal.jsx

--- src/Components/Users/EdituserModal.jsx
+++ src/Components/Users/EdituserModal.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React from "react";
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Button
-} from '@mui/material';
+   Dialog,
+   DialogTitle,
+   DialogContent,
+   DialogContentText,
+   DialogActions,
+   Button,
+} from "@mui/material";
 
 const EdituserModal = ({ open, onClose, onConfirm, user }) => {
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Confirm Edit</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Are you sure you want to Edit{' '}
-          <strong>{user?.name || user?.gmail || 'this user'}</strong>?
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} variant="outlined">
-          Cancel
-        </Button>
-        <Button onClick={() => onConfirm(user._id)} color="primary" variant="contained">
-          Edit
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+   return (
+      <Dialog open={open} onClose={onClose}>
+         <DialogTitle>Confirm Edit</DialogTitle>
+         <DialogContent>
+            <DialogContentText>
+               Are you sure you want to Edit{" "}
+               <strong>{user?.name || user?.gmail || "this user"}</strong>?
+            </DialogContentText>
+         </DialogContent>
+         <DialogActions>
+            <Button onClick={onClose} variant="outlined">
+               Cancel
+            </Button>
+            <Button
+               onClick={() => onConfirm(user._id)}
+               color="primary"
+               variant="contained">
+               Edit
+            </Button>
+         </DialogActions>
+      </Dialog>
+   );
 };
 
 export default EdituserModal;
